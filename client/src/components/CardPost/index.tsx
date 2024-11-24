4689363f0cb984e0061738a3c69acb32c5d05d79
Gio <giovanicarvalhoavila@hotmail.com>
2024-11-24 13:42:39 -0300

    fix: realoding page when clicking in the post card

--- client/src/components/CardPost/index.tsx
+++ client/src/components/CardPost/index.tsx
@@ -1,6 +1,7 @@
-import { Heading, Text, Image, Flex } from "@chakra-ui/react";
+import { Heading, Text, Image, Flex, Spinner } from "@chakra-ui/react";
 import comments from "../../assets/comments.svg";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 interface CardPostProps {
   id: string;
@@ -8,18 +9,16 @@ interface CardPostProps {
   content: string;
 }
 
-function navigateTo(id: string) {
+export default function CardPost({ id, title, content }: CardPostProps) {
+  const [isLoading, setLoading] = useState(false); 
   const navigate = useNavigate();
 
   function handleClick() {
+    setLoading(true); 
     navigate(`posts/${id}`); 
+    window.location.reload();
   }
 
-  return { handleClick };
-}
-
-export default function CardPost({ id, title, content }: CardPostProps) {
-  const { handleClick } = navigateTo(id);
   return (
     <Flex
       direction="column"
@@ -32,18 +31,26 @@ export default function CardPost({ id, title, content }: CardPostProps) {
         transform: "translateX(6px)",
         transition: "transform 0.2s ease-in-out",
       }}
-      onClick={handleClick}
+      onClick={handleClick} 
     >
-      <Heading as="h2" fontSize="25px" fontWeight="600">
-        {title}
-      </Heading>
-      <Text fontSize="15px" color="gray.500">
-        {content}
-      </Text>
-      <Flex gap="10px">
-        <Image src={comments} />
-        <Text color="gray.500">See comments</Text>
-      </Flex>
+      {isLoading ? (
+        <Flex justify="center" align="center" h="200px">
+          <Spinner size="xl" />
+        </Flex>
+      ) : (
+        <>
+          <Heading as="h2" fontSize="25px" fontWeight="600">
+            {title}
+          </Heading>
+          <Text fontSize="15px" color="gray.500">
+            {content}
+          </Text>
+          <Flex gap="10px">
+            <Image src={comments} />
+            <Text color="gray.500">See comments</Text>
+          </Flex>
+        </>
+      )}
     </Flex>
   );
 }
