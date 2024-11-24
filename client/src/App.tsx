0a77bdee4183233e33733c067ff6a0152c53b622
Gio <giovanicarvalhoavila@hotmail.com>
2024-11-24 13:41:58 -0300

    style: comment react query devtools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 import MainRoutes from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "./context/UserContext";
 
@@ -16,7 +15,7 @@ function App() {
           <MainRoutes />
           <ToastContainer />
         </ChakraProvider>
-       
+        {/* <ReactQueryDevtools initialIsOpen={true} /> */}
       </UserProvider>
     </QueryClientProvider>
   );
